47e33b3945147425fd671ad63912dbede46f1540
emapg <emap04@proton.me>
2024-09-10 13:23:42 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,3 +45,33 @@ $(function(){
 	);
 
 });
+
+    function getVideo() {
+      $.ajax({
+        type: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            key: 'AIzaSyAJEN4-wOoJdHHLmiCAAH9hduSWXiN6L6s',
+            q: "Bangla+Natok",
+            part: 'snippet',
+            maxResults: 5,
+            type: 'video',
+            videoEmbeddable: true,
+        },
+        success: function(data){
+            embedVideo(data)
+        },
+        error: function(response){
+            console.log("Request Failed");
+        }
+      });
+    }
+
+function embedVideo(data) {
+    $('img').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+    $('captatx').text(data.items[0].snippet.title)
+    $('.description').text(data.items[0].snippet.description)
+    $('.date').text(data.items[0].snippet.publishedAt)
+}
+
+getVideo();
